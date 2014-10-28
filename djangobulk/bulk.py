9e1d7926446160bc558fc6f3ab6cd9e9c4be5ae3
Luka Kladaric <allixsenos@gmail.com>
2014-10-28 02:37:59 +0100

    first whack at CTE UPSERT

--- djangobulk/bulk.py
+++ djangobulk/bulk.py
@@ -1,8 +1,10 @@
 '''
 Django bulk operations on simple models.
-Does not attempt to cover all corner cases and related models.
 
-Originally from http://people.iola.dk/olau/python/bulkops.py
+inspired by:
+https://dba.stackexchange.com/questions/13468/most-idiomatic-way-to-implement-upsert-in-postgresql-nowadays/13477#13477?s=f2acc8471f96486aaf0a5d6247241cf3
+http://stackoverflow.com/questions/1109061/insert-on-duplicate-update-in-postgresql/8702291#8702291
+http://www.the-art-of-web.com/sql/upsert/
 
 '''
 from itertools import repeat
@@ -19,112 +21,15 @@ def _prep_values(fields, obj, con):
                  for f in fields)
 
 
-def _insert_many(model, objects, using="default"):
-    if not objects:
-        return
-
-    con = connections[using]
-
-    fields = _model_fields(model)
-    parameters = [_prep_values(fields, o, con) for o in objects]
-
-    table = model._meta.db_table
-    col_names = ",".join(con.ops.quote_name(f.column) for f in fields)
-    placeholders = ",".join(repeat("%s", len(fields)))
-
-    sql = "INSERT INTO %s (%s) VALUES (%s)" % (table, col_names, placeholders)
-    con.cursor().executemany(sql, parameters)
-
-
-def insert_many(*args, **kwargs):
+def insert_or_update_many(model, objects, keys=None, skip_for_update=[], using="default"):
     '''
-    Bulk insert list of Django objects. Objects must be of the same
-    Django model.
-
-    Note that save is not called and signals on the model are not
-    raised.
-
-    :param model: Django model class.
-    :param objects: List of objects of class `model`.
-    :param using: Database to use.
-
-    '''
-    _insert_many(*args, **kwargs)
-    transaction.commit_unless_managed()
-
-
-def _update_many(model, objects, keys=None, using="default"):
-    if not objects:
-        return
-
-    # If no keys specified, use the primary key by default
-    keys = keys or [model._meta.pk.name]
-
-    con = connections[using]
-
-    # Split the fields into the fields we want to update and the fields we want
-    # to update by in the WHERE clause.
-    key_fields = [f for f in model._meta.fields if f.name in keys]
-    value_fields = [f for f in _model_fields(model) if f.name not in keys]
-
-    assert key_fields, "Empty key fields"
-
-    # Combine the fields for the parameter list
-    param_fields = value_fields + key_fields
-    parameters = [_prep_values(param_fields, o, con) for o in objects]
-
-    # Build the SQL
-    table = model._meta.db_table
-    assignments = ",".join(("%s=%%s" % con.ops.quote_name(f.column))
-                           for f in value_fields)
-    where_keys = " AND ".join(("%s=%%s" % con.ops.quote_name(f.column))
-                              for f in key_fields)
-    sql = "UPDATE %s SET %s WHERE %s" % (table, assignments, where_keys)
-    con.cursor().executemany(sql, parameters)
-
-
-def update_many(*args, **kwargs):
-    '''
-    Bulk update list of Django objects. Objects must be of the same
-    Django model.
-
-    Note that save is not called and signals on the model are not
-    raised.
+    Bulk UPSERT (UPDATE or INSERT) a list of Django objects. This uses postgres
+    CTEs so it only works with postgres.
 
     :param model: Django model class.
     :param objects: List of objects of class `model`.
     :param keys: A list of field names to update on.
-    :param using: Database to use.
-
-    '''
-    _update_many(*args, **kwargs)
-    transaction.commit_unless_managed()
-
-
-def _filter_objects(con, objects, key_fields):
-    '''Fitler out objects with duplicate key fields.'''
-    keyset = set()
-
-    # reverse = latest wins
-    for o in reversed(objects):
-        okeys = _prep_values(key_fields, o, con)
-        if okeys in keyset:
-            continue
-        keyset.add(okeys)
-        yield o
-
-
-def insert_or_update_many(model, objects, keys=None, using="default"):
-    '''
-    Bulk insert or update a list of Django objects. This works by
-    first selecting each object's keys from the database. If an
-    object's keys already exist, update, otherwise insert.
-
-    Does not work with SQLite as it does not support tuple comparison.
-
-    :param model: Django model class.
-    :param objects: List of objects of class `model`.
-    :param keys: A list of field names to update on.
+    :param skip_for_update: Fields to skip when updating (for instance created_timestamp)
     :param using: Database to use.
 
     '''
@@ -133,37 +38,90 @@ def insert_or_update_many(model, objects, keys=None, using="default"):
 
     keys = keys or [model._meta.pk.name]
     con = connections[using]
+
+    table = model._meta.db_table
+
+    all_fields = _model_fields(model)
+
+    # these are the fields that will be INSERTed on a failed UPDATE
+    all_field_names = [f.name for f in all_fields]
+    all_col_names = ",".join(con.ops.quote_name(f.column) for f in all_fields)
+
+    # key fields are those used for WHERE in the UPDATE
+    key_fields = [f for f in model._meta.fields if f.name in keys and f.name in all_field_names]
+    key_col_names = ",".join(con.ops.quote_name(f.column) for f in key_fields)
 
     # Select key tuples from the database to find out which ones need to be
     # updated and which ones need to be inserted.
-    key_fields = [f for f in model._meta.fields if f.name in keys]
     assert key_fields, "Empty key fields"
 
-    object_keys = [(o, _prep_values(key_fields, o, con)) for o in objects]
-    parameters = [i for (_, k) in object_keys for i in k]
-
-    table = model._meta.db_table
-    col_names = ",".join(con.ops.quote_name(f.column) for f in key_fields)
+    # update fields are those whose values are updated
+    update_fields = [f for f in model._meta.fields if f.name not in keys and f.name not in skip_for_update and f.name in all_field_names]
+    update_col_names = ",".join(con.ops.quote_name(f.column) for f in update_fields)
 
     # repeat tuple values
-    tuple_placeholder = "(%s)" % ",".join(repeat("%s", len(key_fields)))
+    # tuple_placeholder = "(%s)" % ",".join(repeat("%s", len(all_fields)))
+    tuple_placeholder = "(%s)" % ",".join("%%s::%s" % f.db_type(con) for f in all_fields)  # TODO: the type decoration is only necessary for the first row...
     placeholders = ",".join(repeat(tuple_placeholder, len(objects)))
 
-    sql = "SELECT %s FROM %s WHERE (%s) IN (%s)" % (
-        col_names, table, col_names, placeholders)
+    parameters = [_prep_values(all_fields, o, con) for o in objects]
+    parameters = [field for row in parameters for field in row]  # TODO: de-brainfuck this
+
+    assignments = ",".join("%(f)s=nv.%(f)s" % {
+        'f': con.ops.quote_name(f.column)
+    } for f in update_fields)
+
+    where_keys = " AND ".join("m.%(f)s=nv.%(f)s" % {
+        'f': con.ops.quote_name(f.column)
+    } for f in key_fields)
+
+    up_where_keys = " AND ".join("up.%(f)s=new_values.%(f)s" % {
+        'f': con.ops.quote_name(f.column)
+    } for f in key_fields)
+
+    sql_replacements = dict(
+        keys=keys,
+        table=table,
+        all_fields=all_fields,
+        all_col_names=all_col_names,
+        key_fields=key_fields,
+        key_col_names=key_col_names,
+        update_fields=update_fields,
+        update_col_names=update_col_names,
+        tuple_placeholder=tuple_placeholder,
+        placeholders=placeholders,
+        parameters=parameters,
+        assignments=assignments,
+        where_keys=where_keys,
+        up_where_keys=up_where_keys,
+    )
+
+    # return sql_replacements  # DEBUG
+
+    sql = """
+        WITH new_values (%(all_col_names)s) AS (
+          VALUES
+            %(placeholders)s
+        ),
+        upsert AS
+        (
+            UPDATE %(table)s m
+                SET %(assignments)s
+            FROM new_values nv
+            WHERE %(where_keys)s
+            RETURNING m.*
+        )
+        INSERT INTO %(table)s (%(all_col_names)s)
+        SELECT %(all_col_names)s
+        FROM new_values
+        WHERE NOT EXISTS (SELECT 1
+                          FROM upsert up
+                          WHERE %(up_where_keys)s)
+    """ % sql_replacements
+
+    # return sql, parameters  # DEBUG
+
     cursor = con.cursor()
     cursor.execute(sql, parameters)
-    existing = set(cursor.fetchall())
 
-    # Split the objects that need to be updated and the objects that need to be
-    # inserted.
-    update_objects = [o for (o, k) in object_keys if k in existing]
-    insert_objects = [o for (o, k) in object_keys if k not in existing]
-
-    _update_many(model, update_objects, keys=keys, using=using)
-
-    # Filter out any duplicates in the insertion
-    filtered_objects = _filter_objects(con, insert_objects, key_fields)
-
-    _insert_many(model, filtered_objects, using=using)
     transaction.commit_unless_managed()
